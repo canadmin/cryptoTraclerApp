d7758b1d51351f2cd7b5fe9028d6c4d65dc33b57
canadmin <icanyard@gmail.com>
2022-04-15 21:28:19 +0300

    İos conf

--- src/helper/apiClient.js
+++ src/helper/apiClient.js
@@ -4,8 +4,8 @@ const  {REACT_APP_CRYPTO_API_URL} = process.env;
 
 export const request = (options) => {
   let url = options.params
-    ? `${"http://localhost:4000/"}${options.path}?`
-    : `${"http://localhost:4000/"}${options.path}`;
+    ? `${"http://10.0.2.2:4000/"}${options.path}?`
+    : `${"http://10.0.2.2:4000/"}${options.path}`;
   if (options.params && options.params.length) {
     options.params.map((par, index) => {
       return (url += par + "=" + options.values[index] + "&");
@@ -22,6 +22,22 @@ export const externalApirequest = (options) => {
   let url = options.params
     ? `${"https://data.gateapi.io/api2/1/ticker/"}${options.path}?`
     : `${"https://data.gateapi.io/api2/1/ticker/"}${options.path}`;
+  if (options.params && options.params.length) {
+    options.params.map((par, index) => {
+      return (url += par + "=" + options.values[index] + "&");
+    });
+  }
+  return axios({
+    method: options.method,
+    url: url,
+    data: options.data,
+  })
+};
+
+
+//
+export const getOhlcHistoryRequest = (options) => {
+  let url = `${"https://api.coingecko.com/api/v3/coins/"+options.currency_name_id+"/ohlc"}${options.path}?`
   if (options.params && options.params.length) {
     options.params.map((par, index) => {
       return (url += par + "=" + options.values[index] + "&");
